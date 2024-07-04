2a987959acf806e7a8e13f54cebd1e2b97534c2f
Thiago Conceição <thiago.conceicao@oliveiratrust.com.br>
2024-07-04 13:32:59 -0300

    Python package and module to validate cpfs and cnpj´s using the brazilian format

---
+++ main.py
@@ -0,0 +1,12 @@
+from ValidadorCpfCnpj import ValidadorCpf , ValidadorCnpj
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    validacao = ValidadorCpf("9986017050")
+    validacao_cnpj = ValidadorCnpj(5913698000150)
+
+
